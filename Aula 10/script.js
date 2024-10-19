97920761e3fc7adfc27f1773afd541c1555af17f
Gui-jpng <lucaschicarelli2000@gmail.com>
2024-10-19 15:30:06 -0300

    aula 11

--- Aula 10/script.js
+++ Aula 10/script.js
@@ -18,12 +18,12 @@ const questoes_quiz= [
 
     },
     {
-        questao: '',
-        a: '',
-        b:'',
-        c:'',
-        d:'',
-        correta:''
+        questao: 'qual a empresa de jogos mais antiga',
+        a: 'sega',
+        b:'nintendo',
+        c:'atari',
+        d:'steel wool',
+        correta:'b'
 
     },
 
@@ -35,4 +35,77 @@ const alternativa_b = document.querySelector('#texto_b')
 const alternativa_c = document.querySelector('#texto_c')
 const alternativa_d = document.querySelector('#texto_d')
 const botao = document.querySelector('button')
-const quiz_container = document
+const quiz_container = document.querySelector('.container')
+const alternativas = document.querySelectorAll('.resposta')
+let acertos = 0
+let questao_aual = 0
+
+window.onload = carregar_quiz()
+botao.addEventListener('click', verificar)
+
+function carregar_quiz(){
+    const questao = questoes_quiz[questao_aual]
+    alternativas.forEach((alternativa) => {
+        alternativa.checked = false
+        document.querySelector(`label[for=${alternativa.id}]`).classList.remove('correta', 'incorreta')
+    })
+    quiz_titulo.innerText = questao.questao
+    alternativa_a.innerText = questao.a
+    alternativa_b.innerText = questao.b
+    alternativa_c.innerText = questao.c
+    alternativa_d.innerText = questao.d
+    botao.innerText = 'verificar'
+    verificou = false 
+}
+
+function verificar(){
+    if (verificou == false) {
+        verificar_resposta()
+    }
+    else{
+        proxima_pergunta()
+    }
+}
+
+function verificar_resposta() {
+ const resposta_jogador = pegar_resposta()
+ const resposta_correta = questoes_quiz[questao_aual].correta
+
+ alternativas.forEach ((alternativa) => {
+    const label = document.querySelector(`label[for=${alternativa.id}]`)
+    if (alternativa.id == resposta_correta){
+        label.classList.add('correta')
+    }else{
+        label.classList.add('incorreta')
+    }
+ })
+
+ if (resposta_correta == resposta_jogador){
+    acertos++
+ }
+ botao.innerText = 'proxima'
+ verificou = true
+}
+
+function pegar_resposta(){
+    let respostas = undefined
+
+    alternativas.forEach((alternativa)=>{
+        if (alternativa.checked == true){
+            respostas = alternativa.id
+        }
+    })
+    return respostas
+}
+
+function proxima_pergunta() {
+    questao_aual++
+    maximo_perguntas = questoes_quiz.length
+    if (questao_aual < maximo_perguntas){
+          carregar_quiz()
+    }
+    else{
+        quiz_container.innerHTML =`<h1 id="questao"> Voce acertou ${acertos} de ${maximo_perguntas} perguntas! </h1>`
+    }
+}
+
