f7a36e979590a842c2adc6a6b0f458134488e0f8
baia <miguelcomce009@gmail.com>
2024-12-21 15:20:11 -0300

    aula 14

---
+++ aula 14/script.js
@@ -0,0 +1,9 @@
+const poke_name = document.querySelector('.poke_name')
+const poke_number = document.querySelector('.poke_number')
+const poke_img = document.querySelector ('.poke_img')
+const container_input = document.querySelector ('.container_input')
+const input = document.querySelector('input')
+const prev_btn = document.querySelector('.pre_btn')
+const next_btn = document.querySelector('.next_btn')
+
+let id_pokemon
