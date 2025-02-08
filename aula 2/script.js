9a5d1c1a19ddd391d43278f848ac1c88f3611197
sarah131418 <sarahaureafreitas@gmail.com>
2025-02-08 15:28:02 -0300

    aula 2

--- aula 2/script.js
+++ aula 2/script.js
@@ -1,18 +1,19 @@
 function add_task(){
-    let task = document.querySelector('#input_task').value;
+    let task = document.querySelector("#input_task").value;
     console.log('task')
+let list_task = document.querySelector(".list_task");
     
-    let list_task = document.querySelector('.list_task');
-    
-    let delete_button = document.createElement('button')
+    let new_task = document.createElement("li")
+    new_task.textContent= task;
+
+
+    let delete_button = document.createElement("button")
     delete_button.textContent = "➖"
     delete_button.style.marginLeft = '10px';
-    delete_button.onclick = function() (
+    delete_button.onclick = function() {
         new_task.remove()
-    ;
+};
 
-    let new_task = document.createElement('li')
-    new_task.textContent= task;
     list_task.appendChild(new_task)
 
     document.querySelector = ("#input_task").value = ""
