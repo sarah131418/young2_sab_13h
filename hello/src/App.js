feace33522362b6548b67548b3b7e57ffc6a5536
sarah131418 <sarahaureafreitas@gmail.com>
2025-02-15 15:32:28 -0300

    aula 3

---
+++ hello/src/App.js
@@ -0,0 +1,17 @@
+import logo from './Purple_man.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          hello world
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
