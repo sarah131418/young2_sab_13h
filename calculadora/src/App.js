f32c234cd0970a1a29060750d98d1ab27b7f6219
Gabzn0 <gahgamer2410@gmail.com>
2025-03-08 15:31:36 -0300

    aula-6

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import 'HelloWorld' from './test.jsx';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">eu sou o FNAF!</h1>
+      <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-700 transition">Testar tailwind</button>
     </div>
   );
 }
--- calculadora/src/test.jsx
+++
@@ -1,7 +0,0 @@
-function HelloWorld(){
-    return(
-        <h1 className= "bg-blue-500 text-9x1"> Hello World </h1>
-    )
-}
-
-export default HelloWorld ()
