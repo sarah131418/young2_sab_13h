dc5e6b85215dc8e1c075bea924b67fdfd8d2339d
Gabzn0 <gahgamer2410@gmail.com>
2025-04-26 15:27:48 -0300

    aula 10

---
+++ todo/src/AddQuest.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function AddQuest(props){
+    const [title, setTitle]= useState()
+    return(
+        <div className="flex gap-4 w-full justify-center items-center">
+            <input type="text" placeholder="quest" className="rounded-full bg-secondary pl-2 input-sm flex w-[70%] focus:outline-none" onChange={(e)=> setTitle(e.target.value)}/>
+            <button className=" flex item-center text-center rounded-full bg-primary h-fit px-2 text-lg transform ease-out duration-300" onClick={()=>props.saveAddQuest(title)}>
+                +
+            </button>
+        </div>
+    )
+}
+
+export default AddQuest;
--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
+import AddQuest from './AddQuest'
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="flex h-screen justify-center items-center">
-      <div className="card w-[80%] 1g:w-[50%] h-[70%] shadow-md rounded-sm transforme ease-out duration-300 items-center p-10 gap-5">
-        <h1 className="text-amber-600 text-5xl font-work font-bold w-fit text-center">Quests to do</h1>
+  const [quests, setQuests]= useState({})
+
+  function saveAddQuest(title){
+    let auxQuests = quests
+    let id = 0
+    if(auxQuests.length){
+      id = auxQuests[auxQuests.length -1].id
+    }
+    id ++
+    console.log(title)
+  }
+  return(
+    <div className='flex h-screen justify-center items-center'>
+      <div className='card w-[80%] 1g:w-[50%] h-[70%] shadow-md rounded-sm transform ease-out duration-300 items-center p-10 gap-5'>
+        <h1 className='text-5xl font-work font-bold w-fit text-center'>Quests to do</h1>
+        <AddQuest saveAddQuest={saveAddQuest}/>
       </div>
     </div>
-)
+  )
 }
 
 export default App
